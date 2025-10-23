052e2e183de094cc8e62a13ff6c6bfeb5f7a0cab
D M Shreyas <156510645+dmshreyas28@users.noreply.github.com>
2025-10-23 21:39:55 +0530

    Create new-website branch with only website files (HTML, CSS, JS)

--- android/app/src/main/kotlin/com/example/tagit_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.tagit_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/com/tagit/emergency/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.tagit.emergency
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
